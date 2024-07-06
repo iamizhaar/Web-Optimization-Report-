ddc55b377f31cdd935a6f04882744c3af6bdab8d
iamizhaar <iamisare01@gmail.com>
2024-07-06 21:37:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+    // Simulate a long task
+    setTimeout(() => {
+        console.log('Simulating a long task');
+    }, 5000);
+});
